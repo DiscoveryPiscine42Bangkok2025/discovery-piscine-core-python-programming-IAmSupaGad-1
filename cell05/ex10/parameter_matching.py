de78169379e08b4a353dcda46026a71b81247f5f
IAmSupaGad <90162726+IAmSupaGad@users.noreply.github.com>
2026-04-26 10:48:30 +0700

    Add files via upload

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) != 2:
+    print('none')
+else:
+   t = sys.argv[1]
+   x = input('What was the parameter? ')
+   if x == t:
+     print('Good job!')
+   else:
+     print('Nope, sorry...')
