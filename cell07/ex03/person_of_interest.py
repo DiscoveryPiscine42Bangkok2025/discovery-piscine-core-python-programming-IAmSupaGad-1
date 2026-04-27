4b09c95940c45ec86292910fb478da556c708925
IAmSupaGad <90162726+IAmSupaGad@users.noreply.github.com>
2026-04-27 16:50:17 +0700

    Add files via upload

---
+++ cell07/ex03/person_of_interest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def famous_births(people):
+    sorted_people = sorted(
+        people.items(),
+        key=lambda item: int(item[1]["date_of_birth"])
+    )
+
+    for _, info in sorted_people:
+        print(f"{info['name']} is a great scientist born in {info['date_of_birth']}.")
+
+
+women_scientists = {
+    "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+    "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+    "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+    "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
+
+famous_births(women_scientists)
